738b7cf87962da0796b444ca9d94722d82072cae
louayahmad <58867391+louayahmad@users.noreply.github.com>
2020-09-23 21:51:10 -0400

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,19 +1,8 @@
-package question3;
-
-//********************************************************************
-//  Address.java       Author: Lewis/Loftus
-//
-//  Represents a street address.
-//********************************************************************
-
 public class Address
 {
    private String streetAddress, city, state;
    private long zipCode;
-
-   //-----------------------------------------------------------------
-   //  Constructor: Sets up this address with the specified data.
-   //-----------------------------------------------------------------
+   
    public Address (String street, String town, String st, long zip)
    {
       streetAddress = street;
@@ -22,9 +11,6 @@ public class Address
       zipCode = zip;
    }
 
-   //-----------------------------------------------------------------
-   //  Returns a description of this Address object.
-   //-----------------------------------------------------------------
    public String toString()
    {
       String result;
